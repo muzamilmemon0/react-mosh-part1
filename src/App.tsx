3830bcc944cbbc91da4969b249b28941f33c5d9e
muzamilmemon0 <muzamil.memon315@gmail.com>
2023-07-14 21:11:08 +0500

    immer package

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 import { useState } from "react";
 import Like from "./components/Like";
+import produce from "immer";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
+
+  const [tags, setTags] = useState(["happy", "cheerful"]);
+
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug1", fixed: false },
+    { id: 2, title: "Bug2", fixed: false },
+  ]);
 
   const [customer, setCustomer] = useState({
     name: "John",
@@ -21,11 +29,30 @@ function App() {
     console.log(item);
   };
 
+  // const handleClick = () => {
+  //   // Add
+  //   setTags([...tags, "exciting"]);
+
+  //   // Remove
+  //   setTags(tags.filter((tag) => tag !== "happy"));
+
+  //   // Update
+  //   setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+  // };
+
+  // Update bug object
+  // const handleClick = () => {
+  //   setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+  // };
+
+  // update with immer
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 94112 },
-    });
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed === true;
+      })
+    );
   };
 
   return (
@@ -50,6 +77,12 @@ function App() {
       </Button>
       <Like onClick={() => console.log("clicked")} />
       {customer.address.city} {customer.address.zipCode}
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title}
+          {bug.fixed ? " Fixed" : " New"}
+        </p>
+      ))}
       <button onClick={handleClick} type="submit">
         Click me
       </button>
