effd508dff7b6640f1c1698b7d90c617f8bde990
muzamilmemon0 <muzamil.memon315@gmail.com>
2023-07-14 11:22:05 +0500

    modules

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
+
+function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          hello <span>world!</span>
+        </Alert>
+      )}
+      <Button color="success" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
