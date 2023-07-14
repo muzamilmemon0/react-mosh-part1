ba92535476baa0e62a9753614725660841bc2c66
muzamilmemon0 <muzamil.memon315@gmail.com>
2023-07-14 17:42:24 +0500

    updating objects lesson

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import { useState } from "react";
+import Like from "./components/Like";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
-  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
+  };
+
   return (
     <>
       <ListGroup
@@ -21,9 +34,19 @@ function App() {
           hello <span>world!</span>
         </Alert>
       )}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+      <Button
+        color="primary"
+        onClick={() => {
+          setAlertVisibility(true);
+        }}
+      >
         My Button
       </Button>
+      <Like onClick={() => console.log("clicked")} />
+      {drink.price}
+      <button onClick={handleClick} type="submit">
+        Click me
+      </button>
     </>
   );
 }
